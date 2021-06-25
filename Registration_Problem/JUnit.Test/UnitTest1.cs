33736cde3d6b163fe35dbc4e5f29eeef9f171d81
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-25 22:37:58 +0530

    [akhila]Added:TestCases

--- Registration_Problem/JUnit.Test/UnitTest1.cs
+++ Registration_Problem/JUnit.Test/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace JUnit.Test
     public class Tests
     { 
         [Test]
-        public void GivenUserDetailsReturnsSuccesfulEntry()
+        public void GivenHappyUserDetailsReturnsSuccesfulEntry()
         {
             Registration registration = new Registration();
             bool firstname = registration.ValidateFirstName("Akhila");
@@ -16,6 +16,18 @@ namespace JUnit.Test
             bool password = registration.ValidatePassword("Akhi@12b");            
             //Assert
             Assert.Pass("EntrySuccesful");
+        }
+        [Test]
+        public void GivenSadUserDetailsReturnFailEntry()
+        {
+            Registration registration = new Registration();
+            bool firstname = registration.ValidateFirstName("Akhila");
+            bool lastname = registration.ValidateFirstName("Reddy");
+            bool emailid = registration.ValidateEmail("akhila.it@gmilcom");
+            bool phonenumber = registration.ValidatePhonenumber("91 9908556919");
+            bool password = registration.ValidatePassword("Aki@12b");
+            //Assert
+            Assert.Pass("EntryFail");
         }
     }
 }
