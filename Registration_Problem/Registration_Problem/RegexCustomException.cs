17666121392a2e28846f2b9e52a5ab9972be28ea
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-26 22:42:03 +0530

    [akhila]refactor:customexception

---
+++ Registration_Problem/Registration_Problem/RegexCustomException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Registration_Problem
+{
+    public class RegexCustomException : Exception
+    {
+        /// <summary>
+        /// enum for exception type
+        /// </summary>
+        public enum InvalidUserDetails
+        {
+            NULL_MESSAGE, EMPTY_MESSAGE
+        }
+        //creating type variable of type exceptiontype
+        private readonly InvalidUserDetails  type;
+
+        ///<summary>
+        ///parameterized  constructor sets the exception type and message
+        ///</summary>
+        public RegexCustomException(InvalidUserDetails  type, string message) : base(message)
+        {
+            this.type = type;
+        }
+
+    }
+}
--- Registration_Problem/Registration_Problem/Registration.cs
+++ Registration_Problem/Registration_Problem/Registration.cs
@@ -13,20 +13,26 @@ namespace Registration_Problem
         /// <summary>
         /// UC1-validating Firstname with Caps and minimum 3 character
         /// UC2-validating Lastname with caps and minimum 3 cahracters
+        /// validate using custom exceptions
         /// </summary>
         public bool ValidateFirstName(string firstName)
         {
             string ValidateName = "^[A-Za-z]{3}$";
             try
             {
+                if (string.IsNullOrEmpty(ValidateName))
+                {
+                    throw new RegexCustomException(RegexCustomException.InvalidUserDetails.EMPTY_MESSAGE, "Name should not be empty");
+                }
                 if (Regex.IsMatch(firstName, ValidateName))
                 {
-                    Console.WriteLine(" valid firstname");                    
+                    Console.WriteLine(" valid firstname");
+                    return true;
                 }
-                else
-                {
-                    Console.WriteLine("INVALID");                    
-                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new RegexCustomException(RegexCustomException.InvalidUserDetails.NULL_MESSAGE, "Name should not be null");
             }
             catch (Exception ex)
             {
@@ -34,23 +40,31 @@ namespace Registration_Problem
             }
             return false;
         }
+        
         /// <summary>
         /// UC3-validating the valid email id 
+        /// validate using custom exceptions
         /// </summary>        
         public bool ValidateEmail(string Email)
         {
             string ValidateEmail = "^[A-Za-z0-9]+[.+-]{0,1}[0-9a-zA-Z]+@[A-Za-z]+[.][A-Za-z]{2,3}(.[a-zA-Z]{2,3}){0,1}$";
             try
             {
+                if (string.IsNullOrEmpty(ValidateEmail))
+                {
+                    throw new RegexCustomException(RegexCustomException.InvalidUserDetails.EMPTY_MESSAGE, "Email should not be empty");
+                }
                 if (Regex.IsMatch(Email, ValidateEmail))
                 {
                     Console.WriteLine(" valid email id");
-                }
-                else
-                {
-                    Console.WriteLine("INVALID");
+                    return true;
                 }
             }
+            catch (NullReferenceException)
+            {
+                throw new RegexCustomException(RegexCustomException.InvalidUserDetails.NULL_MESSAGE, "Email should not be null");
+            }
+        
             catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
@@ -59,22 +73,28 @@ namespace Registration_Problem
         }
         /// <summary>
         /// UC4-validating the predefined mobile number with country code
+        /// validate using custom exception
         /// </summary>       
         public bool ValidatePhonenumber(string phonenumber)
         {
-            string ValidatePhonenumber = "^[0-9]{1,4}\\([0-9]{9,10})$";
+            string ValidatePhonenumber = "^[0-9]{1,4}\\s([0-9]{10,12})$";
             try
             {
+                if (string.IsNullOrEmpty(ValidatePhonenumber))
+                {
+                    throw new RegexCustomException(RegexCustomException.InvalidUserDetails.EMPTY_MESSAGE, "phonenumber should not be empty");
+                }
                 if (Regex.IsMatch(phonenumber, ValidatePhonenumber))
                 {
                     Console.WriteLine(" valid phonenumber");
-                }
-                else
-                {
-                    Console.WriteLine("INVALID");
+                    return true;
                 }
             }
-            catch(Exception ex)
+            catch (NullReferenceException)
+            {
+                throw new RegexCustomException(RegexCustomException.InvalidUserDetails.NULL_MESSAGE, "phonenumber should not be null");
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
@@ -85,20 +105,26 @@ namespace Registration_Problem
         /// UC6-Validating the password with atleast one uppercase
         /// Uc7-validating the password with atleast one numeric
         /// UC8-validating the password with spcl cahracters
+        /// validate using custom exception
         /// </summary>       
         public bool ValidatePassword(string password)
         {
             string ValidatePassword = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()]).{8,}$";
             try
             {
+                if (string.IsNullOrEmpty(ValidatePassword))
+                {
+                    throw new RegexCustomException(RegexCustomException.InvalidUserDetails.EMPTY_MESSAGE, "password should not be empty");
+                }
                 if (Regex.IsMatch(password, ValidatePassword))
                 {
                     Console.WriteLine(" valid password");
+                    return true;
                 }
-                else
-                {
-                    Console.WriteLine("INVALID");
-                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new RegexCustomException(RegexCustomException.InvalidUserDetails.NULL_MESSAGE, "password should not be null");
             }
             catch (Exception ex)
             {
